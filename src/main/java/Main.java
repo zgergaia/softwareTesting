8db8798097449e4edccebc67a154b990d66aaef6
zgergaia <zgergaia>
2021-09-25 10:59:22 +0400

    Remove debug lines from Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,5 @@
-
-
 public class Main {
     public static void main(String[] args) {
-//        WebDriverManager.chromedriver().setup();
-//        WebDriver driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//
-//        driver.get("https://google.com");
-//        var text = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
-//        text.click();
-//        text.sendKeys("Pepe the frog");
-//        text.submit();
-//        //driver.quit();
+
     }
 }
